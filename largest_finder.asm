0c8591f924249095d151c2040762293ea6467b6f
ZoyaFathima01 <zoyaafathimaa001@gmail.com>
2025-07-11 16:09:40 -0700

    Update largest_finder.asm

--- largest_finder.asm
+++ largest_finder.asm
@@ -5,37 +5,37 @@ _start:
     mov eax, [num1]
 
     cmp eax, [num2]
-    jg next1
+    jge next1
     mov eax, [num2]
 next1:
 
     cmp eax, [num3]
-    jg next2
+    jge next2
     mov eax, [num3]
 next2:
 
     cmp eax, [num4]
-    jg next3
+    jge next3
     mov eax, [num4]
 next3:
 
     cmp eax, [num5]
-    jg end
+    jge store
     mov eax, [num5]
 
-end:
-    mov [largest], eax      ; Store result
+store:
+    mov [largest], eax
 
-    ; Exit syscall
+    ; Exit
     mov eax, 1
     int 0x80
 
 section .data
-    num1 dd 7
-    num2 dd 13
-    num3 dd 3
-    num4 dd 19
-    num5 dd 11
+    num1 dd 17
+    num2 dd 42
+    num3 dd 13
+    num4 dd 29
+    num5 dd 6
 
 section .bss
     largest resd 1
